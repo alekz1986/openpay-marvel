9b634f435109fd9790f6e4aeb6502dd6a4b88008
Alex Ch <alekznder@gmail.com>
2023-08-10 08:52:16 -0500

    feat: add audit controller

---
+++ src/main/java/coderio/open/pay/marvel/controllers/AuditController.java
@@ -0,0 +1,25 @@
+package coderio.open.pay.marvel.controllers;
+
+import coderio.open.pay.marvel.repository.AuditClientRepository;
+import coderio.open.pay.marvel.repository.entities.AuditClient;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/audit")
+@AllArgsConstructor
+public class AuditController {
+
+    private final AuditClientRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<AuditClient>> getAll() {
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+}
